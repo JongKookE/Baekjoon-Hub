849367cf774d0db04ca89c65032c8aa1fcdc17f8
jongkookE <whdrnrdl789@naver.com>
2024-08-20 19:16:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,24 +1,26 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static StringTokenizer st;
+    static int N, maxWeight, max;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        maxWeight = Integer.parseInt(st.nextToken());
+        dp = new int[maxWeight+1];
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());		
-		int line = Integer.parseInt(st.nextToken());
-		int limit = Integer.parseInt(st.nextToken());
-		int[] memoi = new int[limit + 1];
-		
-		for(int i = 1; i <= line; i++) {
-			st = new StringTokenizer(br.readLine());
-			int weight = Integer.parseInt(st.nextToken());
-			int value = Integer.parseInt(st.nextToken());
-			for(int j = limit; j >= weight; j--) {								
-				memoi[j] = Math.max(memoi[j], memoi[j - weight] + value);
-			}
-		}
-		
-		System.out.println(memoi[limit]);
-	}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            for(int c = maxWeight; c >= weight; c--) dp[c] = Math.max(dp[c], cost + dp[c-weight]);
+        }
+        System.out.println(dp[maxWeight]);
+
+    }
 }
