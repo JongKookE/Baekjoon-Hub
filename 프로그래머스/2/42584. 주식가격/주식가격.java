a90a5ed8228d89c2f2581ad4a232fb06426d8ca1
jongkookE <whdrnrdl789@naver.com>
2024-09-01 13:49:57 +0900

    [level 2] Title: 주식가격, Time: 23.44 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,18 +4,11 @@ class Solution {
         int[] answer = new int[size];
 
         for(int i = 0; i < size; i++){
-            int cnt = 0;
             for(int j = i+1; j < size; j++){
-                cnt++;
-                if(prices[i] > prices[j]){
-                    answer[i] = cnt;
-                    break;
-                }
+                answer[i]++;
+                if(prices[i] > prices[j]) break;
             }
-            if(answer[i] == 0) answer[i] = size - i - 1;
         }
-
-
         return answer;
     }
 }
