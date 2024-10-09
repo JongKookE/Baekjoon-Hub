556eba050c7f7af5b631802edeafea7322b69d6d
jongkookE <whdrnrdl789@naver.com>
2024-10-09 18:06:06 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 15232 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,44 +2,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long[] array;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        array = new long[N];
-
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int n = 0; n < N; n++) array[n]= Long.parseLong(st.nextToken());
-
-
+        for(int n = 0; n < N; n++) array[n] = Integer.parseInt(st.nextToken());
         Arrays.sort(array);
-
-        for(int i = 0; i < N; i++){
-            long value = array[i];
+        int result = 0;
+        for(int n = 0; n < N; n++){
+            int target = array[n];
             int start = 0;
-            int end = N-1;
+            int end = N - 1;
             while(start < end){
-                long element = array[start] + array[end];
-                if (element == value){
-                    if(start != i && end != i){
-                        cnt++;
+                int currentSum = array[start] + array[end];
+                if(currentSum == target){
+                    if(start != n && end != n){
+                        result++;
                         break;
                     }
-                    else if (start == i) start++;
-                    else if (end == i) end--;
-
+                    else if(start == n) start++;
+                    else end--;
                 }
-                else if (element > value) end--;
+                else if(currentSum > target) end--;
                 else start++;
             }
         }
-        System.out.println(cnt);
+        System.out.println(result);
     }
 }
