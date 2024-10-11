adb8cedc4ff2cafe679e189b0e0092e9534c043c
jongkookE <whdrnrdl789@naver.com>
2024-10-11 14:22:20 +0900

    [level 2] Title: 타겟 넘버, Time: 11.82 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,16 @@
+class Solution {
+    int answer;
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+
+        return answer;
+    }
+    void dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.length){
+            if(sum == target) answer++;
+            return;
+        }
+        dfs(depth+1, sum+numbers[depth], numbers, target);
+        dfs(depth+1, sum-numbers[depth], numbers, target);
+    }
+}
