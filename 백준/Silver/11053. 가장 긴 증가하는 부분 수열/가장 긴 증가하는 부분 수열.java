087476bb7a5a9472e88fffdff2d671c9c5c51638
jongkookE <whdrnrdl789@naver.com>
2024-10-26 17:15:41 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 132 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,29 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 
 public class Main {
-
+	static int N, max;
+	static int[] dp, array;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = Integer.parseInt(br.readLine());
-		int[] array = new int[count];
+		N = Integer.parseInt(br.readLine());
+		dp = new int[N+1];
+		array = new int[N+1];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int n = 1; n <= N; n++) array[n] = Integer.parseInt(st.nextToken());
 
-		for(int i = 0; i < count; i++) array[i] = Integer.parseInt(st.nextToken());
-		int[] dp = new int[count];
-		dp[0] = 1;
-
-		for(int i = 1; i < count; i++){
+		for(int i = 1; i <= N; i++){
 			dp[i] = 1;
-			for(int j = 0; j < i; j++){
-				if(array[i] > array[j] && dp[i] <= dp[j]) dp[i] = dp[j]+1;
+			for(int j = 1; j <= i; j++){
+				if(array[i] > array[j]) dp[i] = Math.max(dp[i], dp[j]+1);
 			}
+			max = Math.max(max, dp[i]);
 		}
-		int max = Integer.MIN_VALUE;
-		for(int d : dp) max = Math.max(max, d);
 		System.out.println(max);
 	}
 
