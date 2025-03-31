acd314b71b9b8f4c8beea36f6c4a0296854519a2
jongkookE <whdrnrdl789@naver.com>
2025-04-01 02:47:39 +0900

    [level 2] Title: 교점에 별 만들기, Time: 23.00 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,85 +1,75 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
-	
-    // 1
-	private static class Point { 
-		
-		public final long x, y; 
-		
-		private Point(long x, long y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-    // 2
-	public static Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
-		double x = (double)(b1*c2 - b2*c1) / (a1*b2 - a2*b1);
-		double y = (double)(a2*c1 - a1*c2) / (a1*b2 - a2*b1);
-		
-		if((x % 1 != 0) || (y % 1 != 0)) {
-			return null;
-		}
-		return new Point((long)x, (long)y);
-	}
-	
+    // 최대 범위는 1,000 * 1,000 범위 이내 표현됌
+    static long maxY = Long.MIN_VALUE, maxX = Long.MIN_VALUE, minY = Long.MAX_VALUE, minX = Long.MAX_VALUE;
     public String[] solution(int[][] line) {
-    	
-    	List<Point> points = new ArrayList<>();
-    	
-    	// 3
-    	for(int i = 0; i < line.length; i++) {
-    		for(int j = i+1; j <line.length; j++) {
-    			Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
-    			
-    			
-    			if(intersection != null) {
-    				points.add(intersection);
-    			}
-    		}
-    	}
-    	
-    	
-    	// 4
-    	long maxX = Long.MIN_VALUE;
-    	long maxY = Long.MIN_VALUE;
-    	long minX = Long.MAX_VALUE;
-    	long minY = Long.MAX_VALUE;
-    	for(int i = 0; i <points.size(); i++) {
-    		maxX = Math.max(maxX, points.get(i).x);
-    		minX = Math.min(minX, points.get(i).x);
-    		maxY = Math.max(maxY, points.get(i).y);
-    		minY = Math.min(minY, points.get(i).y);
-    	}
-    	
-    	// 5
-    	int xLength = (int)(maxX - minX + 1);
-    	int yLength = (int)(maxY - minY + 1);
-    	
-    	char[][] map = new char[yLength][xLength];
-    	
-    	// 6
-    	for(int i = 0; i < yLength; i++) {
-    		for(int j = 0; j < xLength; j++) {
-    			map[i][j] = '.';
-    		}
-    	}
-    	
-    	for(int i = 0; i <points.size(); i++) {
-            int x = (int)(points.get(i).x - minX);
-            int y = (int)(maxY-points.get(i).y);
-    		map[y][x] = '*';
-    	}
-    	
-    	
-    	
-        String[] answer = new String[map.length];
-        for(int i = 0; i < answer.length; i++) {
-        	answer[i] = new String(map[i]);
+        String[] answer = {};
+        ArrayList<Node> nodes = new ArrayList<>();
+
+        for(int i = 0; i < line.length; i++) {
+            for(int j = i+1; j < line.length; j++) {
+                Node node = setPoint(line[i], line[j]);
+                if(node == null) continue;
+                nodes.add(node);
+                setInitialValue(node);
+            }
         }
-        
-        return answer;
+        int lenY = (int) (maxY - minY + 1);
+        int lenX = (int) (maxX - minX + 1);
+
+        char[][] chars = new char[lenY][lenX];
+        for(char[] ch : chars) Arrays.fill(ch, '.');
+
+        return drawGraph(chars, nodes);
+    }
+
+    public void setInitialValue(Node node){
+        maxY = Math.max(node.y, maxY);
+        maxX = Math.max(node.x, maxX);
+        minY = Math.min(node.y, minY);
+        minX = Math.min(node.x, minX);
+    }
+
+    public String[] drawGraph(char[][] graph, ArrayList<Node> nodes) {
+        for(Node node : nodes){
+            int y = (int) (maxY - node.y);
+            int x = (int) (node.x - minX);
+
+            graph[y][x] = '*';
+        }
+        return Arrays.stream(graph)
+                .map(String::new)
+                .toArray(String[]::new);
+    }
+
+    public Node setPoint(int[] curr, int[] next){
+        long a = curr[0], c = next[0];
+        long b = curr[1], d = next[1];
+        long e = curr[2], f = next[2];
+
+        double y = (double) (e * c - a * f) / (a * d - b * c);
+        double x = (double) (b * f - e * d) / (a * d - b * c);
+        if(y % 1 != 0 || x % 1 != 0) return null;
+
+        return new Node((long) y, (long) x);
+    }
+
+    static class Node{
+        long y, x;
+
+        public Node(long y, long x) {
+            this.y = y;
+            this.x = x;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "y=" + y +
+                    ", x=" + x +
+                    '}';
+        }
     }
 }
