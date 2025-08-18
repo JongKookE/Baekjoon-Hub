e727b2aac057788296ac7e8aea16694c098a1555
jongkookE <whdrnrdl789@naver.com>
2025-08-19 00:42:05 +0900

    [Bronze V] Title: A+B - 9, Time: 244 ms, Memory: 17280 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -1,10 +1,13 @@
 import java.util.*;
 import java.io.*;
+import java.math.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.println(a.add(b));
     }
 }
