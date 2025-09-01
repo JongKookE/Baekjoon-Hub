e4911a2213602d7fa04a0e6f3fb6ecaf78e9bf92
jongkookE <whdrnrdl789@naver.com>
2025-09-01 15:19:06 +0900

    [Silver II] Title: 크로스워드, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1706. 크로스워드/크로스워드.java
+++ 백준/Silver/1706. 크로스워드/크로스워드.java
@@ -15,25 +15,31 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         String[] horizonWords = new String[row];
-        String[] verticalWords = new String[col];
-        Arrays.fill(verticalWords, "");
+        StringBuilder[] verticalWords = new StringBuilder[col];
+
+        for(int c = 0; c <col; c++) verticalWords[c] = new StringBuilder();
         for(int r = 0; r < row; r++) horizonWords[r] = br.readLine();
 
         for(int c = 0; c < col; c++){
             for(int r = 0; r < row; r++){
-                verticalWords[c] += horizonWords[r].charAt(c);
+                verticalWords[c].append(horizonWords[r].charAt(c));
             }
         }
 
         extractWords(pq, horizonWords);
         extractWords(pq, verticalWords);
+
+//        while(!pq.isEmpty()) System.out.println(pq.poll());
         System.out.println(pq.poll());
     }
     static void extractWords(PriorityQueue<String> pq, String[] words){
-        for(String word : words){
-            insertSplitStr(pq, word);
-        }
+        for(String word : words) insertSplitStr(pq, word);
     }
+
+    static void extractWords(PriorityQueue<String> pq, StringBuilder[] words){
+        for(StringBuilder word : words) insertSplitStr(pq, word.toString());
+    }
+
     static void insertSplitStr(PriorityQueue<String> pq, String word){
         String[] splitWord = word.split("#");
         for(String split : splitWord){
