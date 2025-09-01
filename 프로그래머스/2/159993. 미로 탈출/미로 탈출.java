a4a80b376648d3002d460f8e554140a5ea9a802a
jongkookE <whdrnrdl789@naver.com>
2025-09-01 11:27:40 +0900

    [level 2] Title: 미로 탈출, Time: 5.37 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,7 +1,6 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.Arrays;
 
 class Solution {
     int row, col;
@@ -15,8 +14,6 @@ class Solution {
         row = maps.length;
         col = maps[0].length();
         chMaps = new char[row][col];
-        visited = new boolean[row][col];
-        int answer = 0;
         for(int r = 0; r < row; r++){
             String map = maps[r];
             for(int c = 0; c < col; c++){
@@ -26,10 +23,9 @@ class Solution {
                 position.put(ch, new Node(r, c, 0));
             }
         }
-        int findLeverTime = findLever();
+        int findLeverTime = bfs(start, lever, 0);
         if(findLeverTime == -1) return findLeverTime;
-        int findExitTime = findExit(findLeverTime);
-        return findExitTime;
+        return bfs(lever, exit, findLeverTime);
     }
 
     boolean isOverMapRange(int y, int x){
@@ -37,32 +33,12 @@ class Solution {
     }
 
 
-    int findLever(){
+    int bfs(char startingPoint, char destination, int time){
         dq = new ArrayDeque<>();
-        Node startNode = position.get(start);
-        dq.addLast(startNode);
-        while(!dq.isEmpty()){
-            Node node = dq.pollFirst();    
-            for(int d = 0; d < 4; d++){
-                int ny = node.y + dy[d];
-                int nx = node.x + dx[d];
-                if(isOverMapRange(ny, nx) || visited[ny][nx]) continue;
-                char element = chMaps[ny][nx];
-                if(element == wall) continue;
-                if(element == lever) return node.time+1;
-                visited[ny][nx] = true;
-                dq.addLast(new Node(ny, nx, node.time+1));
-            }
-        }
-        return -1;
-    }
-
-    int findExit(int leverTime){
-        dq.clear();
-        for(boolean[] visit : visited) Arrays.fill(visit, false);
-        Node startNode = position.get(lever);
+        visited = new boolean[row][col];
+        Node startNode = position.get(startingPoint);
+        startNode.time = time;
         visited[startNode.y][startNode.x] = true;
-        startNode.time = leverTime;
         dq.addLast(startNode);
         while(!dq.isEmpty()){
             Node node = dq.pollFirst();
@@ -72,7 +48,7 @@ class Solution {
                 if(isOverMapRange(ny, nx) || visited[ny][nx]) continue;
                 char element = chMaps[ny][nx];
                 if(element == wall) continue;
-                if(element == exit) return node.time+1;
+                if(element == destination) return node.time+1;
                 visited[ny][nx] = true;
                 dq.addLast(new Node(ny, nx, node.time+1));
             }
