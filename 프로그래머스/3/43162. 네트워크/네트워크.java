a46ba2e808f5fc473c02a1269d7b71ce972e5d05
jongkookE <whdrnrdl789@naver.com>
2025-09-08 17:49:28 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,23 @@
-class Solution{
+class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] visited = new boolean[n];
+
         for(int i = 0; i < n; i++){
             if(visited[i]) continue;
             dfs(n, i, computers, visited);
             answer++;
         }
+
         return answer;
     }
-    void dfs(int n, int index, int[][] computers, boolean[] visited) {
-        int[] computer = computers[index];
-        for(int i = 0; i < n; i++){
-            if(visited[i] || i == index) continue;
-            if(computer[i] == 0) continue;
-            visited[i] = true;
-            dfs(n, i, computers, visited);
+    void dfs(int n, int i, int[][] computers, boolean[] visited){
+        int[] computer = computers[i];
+        for(int j = 0; j < n; j++){
+            if(computer[j] == 0 || visited[j] || j == i) continue;
+            visited[j] = true;
+            dfs(n, j, computers, visited);
+
         }
     }
 }
