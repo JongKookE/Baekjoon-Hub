dead33855f4f9aceb7587afa4b7d6af0b133bcc0
jongkookE <whdrnrdl789@naver.com>
2025-09-08 14:47:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.79 ms, Memory: 96.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String str : participant) map.put(str, map.getOrDefault(str, 0)+1);
+        for(String str : completion) map.put(str, map.get(str)-1);
+        for(String key : map.keySet()){
+            if(map.get(key) == 0) continue;
+            answer = key;
+            break;
+        }
+        return answer;
+    }
+}
