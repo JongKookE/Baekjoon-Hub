d2934bd70f21ffff238e0a41f0d58b950e96efb6
jongkookE <whdrnrdl789@naver.com>
2025-09-15 13:49:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 56.14 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,30 +2,31 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
-class Solution{
-    PriorityQueue<Integer> minPQ;
-    PriorityQueue<Integer> maxPQ;
+class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        minPQ = new PriorityQueue<>();
-        maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+        int[] answer = {};
+        PriorityQueue<Integer> findMin = new PriorityQueue<>();
+        PriorityQueue<Integer> findMax = new PriorityQueue<>(Collections.reverseOrder());
         StringTokenizer st;
-        for(String operation : operations) {
+        for(String operation : operations){
             st = new StringTokenizer(operation);
-            char op = st.nextToken().charAt(0);
-            int value = Integer.parseInt(st.nextToken());
-            if (op == 'I') {
-                maxPQ.add(value);
-                minPQ.add(value);
-            }
-            else{
-                if (value == 1) minPQ.remove(maxPQ.poll());
-                else maxPQ.remove(minPQ.poll());
+            char command = st.nextToken().charAt(0);
+            int number = Integer.parseInt(st.nextToken());
+            switch(command){
+                case 'I' -> insertNumber(number, findMin, findMax);
+                case 'D' -> deleteNumber(number, findMin, findMax);
             }
         }
-        if(!maxPQ.isEmpty()) answer[0] = maxPQ.peek();
-        if(!minPQ.isEmpty()) answer[1] = minPQ.peek();
-        return answer;
+        if(findMin.isEmpty()) return new int[] {0, 0};
+        return new int[] {findMax.poll(), findMin.poll()};
+    }
+    void insertNumber(int number, PriorityQueue<Integer> findMin, PriorityQueue<Integer> findMax){
+        findMax.add(number);
+        findMin.add(number);
+    }
+
+    void deleteNumber(int number, PriorityQueue<Integer> findMin, PriorityQueue<Integer> findMax){
+        if(number == 1) findMin.remove(findMax.poll());
+        else findMax.remove(findMin.poll());
     }
 }
