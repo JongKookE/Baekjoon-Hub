18da26e7e5e9d00b16896dc8b0d23481d2e36e77
jongkookE <whdrnrdl789@naver.com>
2025-09-16 17:42:38 +0900

    [level 2] Title: 소수 찾기, Time: 53.16 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,31 +1,46 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 class Solution {
-        boolean[] visited = new boolean[7];
+    int max = Integer.MIN_VALUE;
+    public int solution(String numbers) {
+        int size = numbers.length();
+        int[] src = new int[size];
         HashSet<Integer> set = new HashSet<>();
-        public int solution(String numbers) {
-            int answer = 0;
-            int len = numbers.length();
-            makePermutation(numbers.toCharArray(), "", 0, len);
-            for(int check: set) if(isPrime(check)) answer++;
-            return answer;
-        }
-        void makePermutation(char[] numbers, String str, int depth, int len){
-            if(depth > len) return;
-            for(int i = 0; i < len; i++){
-                if(visited[i]) continue;
-                visited[i] = true;
-                String newStr = str + numbers[i];
-                set.add(Integer.parseInt(newStr));
-                makePermutation(numbers, newStr, depth+1, len);
-                visited[i] = false;
-            }
-        }
+        boolean[] visited = new boolean[size];
+        for(int i = 0; i < size; i++) src[i] = numbers.charAt(i) - '0';
+        makePermutation(numbers.toCharArray(), new StringBuilder(), set, visited, 0);
+        return getPrimeCount(set);
+    }
 
-        boolean isPrime(int number){
-            if(number < 2) return false;
-            for(int i = 2; i <= Math.sqrt(number); i++) if(number % i == 0) return false;
+    int getPrimeCount(HashSet<Integer> set){
+        int answer = 0;
+        boolean[] isPrime = new boolean[max+1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
 
-            return true;
+        for (int i = 2; i * i <= max; i++)
+            if (isPrime[i])
+                for (int j = i * i; j <= max; j += i)
+                    isPrime[j] = false;
+
+        for(int v : set) if(isPrime[v]) answer++;
+        return answer;
+    }
+
+    void makePermutation(char[] src, StringBuilder sb, HashSet<Integer> set, boolean[] visited, int depth){
+        if(depth == src.length) return;
+
+        for(int i = 0; i < src.length; i++){
+            if(visited[i]) continue;
+            visited[i] = true;
+            sb.append(src[i]);
+            int permValue = Integer.parseInt(sb.toString());
+            set.add(permValue);
+            max = Math.max(permValue, max);
+            makePermutation(src, sb, set, visited, depth+1);
+            visited[i] = false;
+            sb.setLength(sb.length()-1);
         }
     }
+}
