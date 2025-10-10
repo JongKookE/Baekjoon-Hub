a0221df1ac3c6c715ca652685a68191b2c8929f9
jongkookE <whdrnrdl789@naver.com>
2025-10-10 18:48:06 +0900

    [Bronze I] Title: 단어 공부, Time: 328 ms, Memory: 37768 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -9,12 +9,12 @@ public class Main {
         HashMap<Character, Integer> map = new HashMap<>();
         char[] chs = br.readLine().toCharArray();
         for(char ch : chs){
-            char lowerCh = Character.toLowerCase(ch);
-            map.put(lowerCh, map.getOrDefault(lowerCh, 0)+1);
+            char upperCh = Character.toUpperCase(ch);
+            map.put(upperCh, map.getOrDefault(upperCh, 0)+1);
         }
         List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
         list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-        if(list.size() >= 2) System.out.println(Objects.equals(list.get(0).getValue(), list.get(1).getValue()) ? '?' : Character.toUpperCase(list.get(0).getKey()));
-        else System.out.println(Character.toUpperCase(list.get(0).getKey()));
+        if(list.size() >= 2) System.out.println(Objects.equals(list.get(0).getValue(), list.get(1).getValue()) ? '?' : list.get(0).getKey());
+        else System.out.println(list.get(0).getKey());
     }
 }
